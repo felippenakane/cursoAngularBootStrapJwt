794e549ba4db129753f7997241bb0bcc868acbcb
felippenakane <andre.nakane@gmail.com>
2019-02-27 08:03:42 -0300

    Aula 24

--- ServiceTest/src/app/app.module.ts
+++ ServiceTest/src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
+import { CommentService } from './comments/comment.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, CommentsComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [CommentService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
